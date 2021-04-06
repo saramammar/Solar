4e73e1ef5510c63c301506968a1eb9257a91f5d9
SaraAmmar <chemist.s.ammar@gmail.com>
2021-04-07 01:57:04 +0200

    Style and Scale Bubbles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BubblesComponent } from './bubbles/bubbles.component';
@@ -10,7 +11,8 @@ import { BubblesComponent } from './bubbles/bubbles.component';
     BubblesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
